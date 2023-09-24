44890bdcb4dd0dc7f1f514aecd9144f780ed6e3e
fupsi42 <fupsi42@mailbox.org>
2023-09-24 14:18:35 +0200

    Change serialnumber format and start

--- myASNlabels.py
+++ myASNlabels.py
@@ -2,11 +2,11 @@ import AveryLabels
 from reportlab.lib.units import mm, cm
 from reportlab_qrcode import QRCodeImage
 
-startASN = 190
+startASN = 1
 
 def render(c,x,y):
     global startASN
-    barcode_value = f"ASN{startASN:05d}"
+    barcode_value = f"ASN2022-{startASN:03d}"
     startASN = startASN + 1
 
     qr = QRCodeImage(barcode_value, size=y*0.9)
